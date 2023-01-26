2e6e942cc3475ac60a59b1b0f320052567d98c0d
XC-Qan <60663465+XC-Qan@users.noreply.github.com>
2023-01-26 17:16:10 +0800

    第一个Beta版！

--- app/src/main/java/cn/spacexc/wearbili/activity/other/AboutActivity.kt
+++ app/src/main/java/cn/spacexc/wearbili/activity/other/AboutActivity.kt
@@ -23,7 +23,7 @@ import cn.spacexc.wearbili.utils.ToastUtils
 //import kotlinx.coroutines.launch
 //import androidx.lifecycle.lifecycleScope
 
-const val APP_VERSION = "Rel-AL 0.22.4"
+const val APP_VERSION = "Rel-BE 1.0.0"
 
 class AboutActivity : AppCompatActivity() {
     private val firstList = listOf(
