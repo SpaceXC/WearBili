615b434772b5becacabca1faf1624aa48beb74bc
XC-Qan <60663465+XC-Qan@users.noreply.github.com>
2023-01-26 17:47:31 +0800

    fix: 修复动态刷新会越来越早的问题

--- app/src/main/java/cn/spacexc/wearbili/activity/other/AboutActivity.kt
+++ app/src/main/java/cn/spacexc/wearbili/activity/other/AboutActivity.kt
@@ -23,7 +23,7 @@ import cn.spacexc.wearbili.utils.ToastUtils
 //import kotlinx.coroutines.launch
 //import androidx.lifecycle.lifecycleScope
 
-const val APP_VERSION = "Rel-BE 1.0.0"
+const val APP_VERSION = "Rel-BE 1.0.1"
 
 class AboutActivity : AppCompatActivity() {
     private val firstList = listOf(
